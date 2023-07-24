6998dcd801837324ba99be56900e72c9b1c5618f
Raimundo Néto <raimundormneto@outlook.com>
2023-07-24 20:50:27 -0300

    version 2.0
    criação de pasta e arquivo da calculadora

--- calculoDataNascimento.py
+++ calculoDataNascimento.py
@@ -1,24 +1,16 @@
-import datetime
-
-def obter_ano_de_nascimento():
-    while True:
-        try:
-            ano = int(input("Digite o ano de nascimento (entre 1922 e 2021): "))
-            if 1922 <= ano <= 2021:
-                return ano
-            else:
-                print("Ano inválido. Tente novamente.")
-        except ValueError:
-            print("Digite um valor numérico válido.")
-
-def calcular_idade(nome, ano_de_nascimento):
-    ano_atual = datetime.date.today().year
-    idade = ano_atual - ano_de_nascimento
-    print(f"{nome}, você completou ou completará {idade} anos em {ano_atual}.")
-
-def main():
-    nome = input("Digite seu nome completo: ")
-    ano_de_nascimento = obter_ano_de_nascimento()
-    calcular_idade(nome, ano_de_nascimento)
-
-main()
+print ("programa IMC")
+rodar = True
+while(rodar):
+    
+    try:
+        print("Digite seu peso")
+        peso = float(input())
+        print("Digite seu altura")
+        alt = float(input())
+        imc = imc_function(peso, alt)
+        print(imc)
+        
+        rodar = False
+    except:
+        print("erro")
+    print("fim") 
