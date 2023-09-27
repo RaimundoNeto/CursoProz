48a2e1fa09fd98fec52f94090221879448a41899
Raimundo Néto <raimundormneto@outlook.com>
2023-09-27 17:15:33 -0300

    Banco de Dados I
    Resolução de exercício de Banco de Dados I - Desenvolvimento.

---
+++ Aula banco de Dados/dbEscola.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE ESCOLA;
+
+CREATE TABLE ALUNO (
+    ID INT AUTO_INCREMET PRIMARY KEY,
+    nome VARCHAR(24),
+    email VARCHAR(24),
+    matricula VARCHAR(24),
+    endereco VARCHAR(55),
+    telefone VARCHAR(15)
+)
